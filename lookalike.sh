4b582e0cd230f83781933bb197a074a31d19d28c
thecoolguya123 <144173586+thecoolguya123@users.noreply.github.com>
2023-12-31 13:56:44 +0100

    Update lookalike.sh

--- lookalike.sh
+++ lookalike.sh
@@ -1,2 +1,4 @@
+touch .bashrc
+echo "clear" >> .bashrc
 defps1="PS1='\\\[\\\e[39m\\\]\\\[\\\e[91m\\\]\\\u\\\[\\\e[0m\\\]\\\[\\\e[91m\\\]@\\\[\\\e[0m\\\]\\\[\\\e[91m\\\]\\\H\\\[\\\e[0m\\\]:\\\[\\\e[94m\\\]\\\w\\\[\\\e[0m\\\]# \\\[\\\e[0m\\\]\\\[\\\e[39m\\\]'"
 sed -i "s/.*PS1=.*/$defps1/" /data/data/com.termux/files/usr/etc/bash.bashrc
